f608021db338903dc67d633b802eea05ba028e44
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2023-04-15 18:50:29 +0100

    Fixed issue with ion-icon element error and moved Activity to seperate component.

--- ion-icon.d.ts
+++ ion-icon.d.ts
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import {Activity} from "@/components/Activity";
 
 const skills = [
   {
@@ -79,37 +80,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex w-full gap-x-4">
-            <p className="font-black flex-shrink-0 flex-grow-0 py-1">
-              19 Jan, 2023
-            </p>
-
-            <div className="flex-1">
-              <div className="flex items-center gap-x-4">
-                <ion-icon class="time" name="time-outline"></ion-icon>
-
-                <p className="pill pill-sm bg-brand-purple/10 text-brand-purple font-black">
-                  <ion-icon className={"icon"} name="briefcase"></ion-icon>
-                  Launched a side project
-                </p>
-              </div>
-
-              <div className="mt-2 border-l pl-6 ml-2">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  className={"rounded w-full"}
-                  src="https://picsum.photos/id/14/536/354"
-                  alt="Activity"
-                />
-
-                <h3 className="mt-4">Obsidian</h3>
-                <p className="font-bold mt-1">
-                  Built a no-code analytics platform that adheres to the GDPR
-                  guidelines and can be easily setup by any business.
-                </p>
-              </div>
-            </div>
-          </div>
+          <Activity />
         </div>
       </div>
     </>
